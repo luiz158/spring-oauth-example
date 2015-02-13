4aae87a1d0d68fe1ec78f9b6629484727470afd3
enr <enrico.comiti@gmail.com>
2015-02-13 17:01:32 +0100

    fix curl client

--- example-clients/terminal/curl-client.sh
+++ example-clients/terminal/curl-client.sh
@@ -1,5 +1,10 @@
-#!/bin/sh
-# Accquire token from the OAuth server with our client credentials
-TOKEN=`curl -s -u curl-client:client-secret localhost:8081/oauth/token\?grant_type=client_credentials | egrep -o '[a-f0-9-]{20,}'`
+#!/usr/bin/env bash
+
+# grep --color=never -Po '"access_token":.*?[^\\]",'
+
+# Acquire token from the OAuth server with our client credentials
+TOKEN=$(curl -X POST --silent --noproxy localhost -u curl-client:client-secret 'localhost:8081/oauth/token?grant_type=client_credentials' -H "Accept: application/json" | egrep --color=never -o '[a-f0-9-]{20,}')
 echo "Got token $TOKEN"
-curl localhost:8080/todos -H "Authorization: Bearer $TOKEN"
+curl --noproxy localhost localhost:8080/todos -H "Authorization: Bearer $TOKEN"
+
+echo ''
