e053b332e08c232668e8d10d3baa3ab2eb9dedc2
Maksim Ustinov <maksimu@users.noreply.github.com>
2015-08-11 12:32:34 -0700

    Update curl-client.sh

--- example-clients/terminal/curl-client.sh
+++ example-clients/terminal/curl-client.sh
@@ -3,3 +3,7 @@
 TOKEN=`curl -s -u curl-client:client-secret localhost:8081/oauth/token\?grant_type=client_credentials | egrep -o '[a-f0-9-]{20,}'`
 echo "Got token $TOKEN"
 curl localhost:8080/todos -H "Authorization: Bearer $TOKEN"
+
+
+# my way
+# curl -s -u curl-client:client-secret localhost:9090/oauth/token\?grant_type=client_credentials -X POST
