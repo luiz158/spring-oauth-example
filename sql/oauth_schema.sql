2e92e25a72575952942414d6f5833513da8e135a
Gautam Roy <gautamr@nrifintech.com>
2016-02-04 11:41:35 +0530

    modified for MySQL dialet

--- sql/oauth_schema.sql
+++ sql/oauth_schema.sql
@@ -1,50 +1,50 @@
 create table oauth_client_details (
-  client_id VARCHAR(256) PRIMARY KEY,
-  resource_ids VARCHAR(256),
-  client_secret VARCHAR(256),
-  scope VARCHAR(256),
-  authorized_grant_types VARCHAR(256),
-  web_server_redirect_uri VARCHAR(256),
-  authorities VARCHAR(256),
+  client_id VARCHAR(100) PRIMARY KEY,
+  resource_ids VARCHAR(100),
+  client_secret VARCHAR(100),
+  scope VARCHAR(100),
+  authorized_grant_types VARCHAR(100),
+  web_server_redirect_uri VARCHAR(100),
+  authorities VARCHAR(100),
   access_token_validity INTEGER,
   refresh_token_validity INTEGER,
-  additional_information VARCHAR(4096),
-  autoapprove VARCHAR(256)
+  additional_information VARCHAR(100),
+  autoapprove VARCHAR(100)
 );
 
 create table oauth_client_token (
-  token_id VARCHAR(256),
-  token LONGVARBINARY,
-  authentication_id VARCHAR(256),
-  user_name VARCHAR(256),
-  client_id VARCHAR(256)
+  token_id VARCHAR(100),
+  token VARBINARY(100),
+  authentication_id VARCHAR(100),
+  user_name VARCHAR(100),
+  client_id VARCHAR(100)
 );
 
 create table oauth_access_token (
-  token_id VARCHAR(256),
-  token LONGVARBINARY,
-  authentication_id VARCHAR(256),
-  user_name VARCHAR(256),
-  client_id VARCHAR(256),
-  authentication LONGVARBINARY,
-  refresh_token VARCHAR(256)
+  token_id VARCHAR(100),
+  token VARBINARY(100),
+  authentication_id VARCHAR(100),
+  user_name VARCHAR(100),
+  client_id VARCHAR(100),
+  authentication VARBINARY(100),
+  refresh_token VARCHAR(100)
 );
 
 create table oauth_refresh_token (
-  token_id VARCHAR(256),
-  token LONGVARBINARY,
-  authentication LONGVARBINARY
+  token_id VARCHAR(100),
+  token VARBINARY(100),
+  authentication VARBINARY(100)
 );
 
 create table oauth_code (
-  code VARCHAR(256), authentication LONGVARBINARY
+  code VARCHAR(100), authentication VARBINARY(100)
 );
 
 create table oauth_approvals (
-userId VARCHAR(256),
-clientId VARCHAR(256),
-scope VARCHAR(256),
-status VARCHAR(10),
+userId VARCHAR(100),
+clientId VARCHAR(100),
+scope VARCHAR(100),
+status VARCHAR(5),
 expiresAt TIMESTAMP,
 lastModifiedAt TIMESTAMP
 );
