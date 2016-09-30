2e501f164c486edbc5553687720e7d2cda60ea73
Moritz Schulze <moritz@frontierpsychiatrist.de>
2016-09-30 20:22:02 +0200

    Upgrade to Spring Boot 1.4.1
    Also
    * Refactores the Gradle projects to share the versioning of the
      Spring Boot Gradle plugin
    * curl client now needs to use POST
    * Fix a URL error in the angular client
    * Use CORS support by Spring instead of the header writer
    * Use new @GetMapping annotations
    * Switches to constructor injection

--- resource-server/src/main/java/de/frontierpsychiatrist/example/oauth/ResourceServerMain.java
+++ resource-server/src/main/java/de/frontierpsychiatrist/example/oauth/ResourceServerMain.java
@@ -1,23 +1,33 @@
 package de.frontierpsychiatrist.example.oauth;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import javax.sql.DataSource;
 
 /**
  * @author Moritz Schulze
  */
-@Configuration
-@ComponentScan
-@EnableAutoConfiguration
+@SpringBootApplication
 public class ResourceServerMain {
+
+    @Configuration
+    static class CorsConfig extends WebMvcConfigurerAdapter {
+
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            // TODO only for OPTIONS requests, but Chrome expect Acess-Control-Allow-Origin on all responses!
+            // TODO or maybe chrome can't accept the fact of 403 and no header?
+            registry.addMapping("/todos/**").allowedMethods("GET", "HEAD", "PUT", "POST", "DELETE");
+        }
+    }
 
     /**
      * Main data source containing the credentials.
